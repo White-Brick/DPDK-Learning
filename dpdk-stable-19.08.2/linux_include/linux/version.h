6f741d63bbfb924e09ec536ae52dc279c0d46f86
NieR <NieR@gmail.com>
2021-09-06 20:57:23 +0800

    delete linux headers

--- dpdk-stable-19.08.2/linux_include/linux/version.h
+++
@@ -1,8 +0,0 @@
-#define LINUX_VERSION_CODE 199168
-#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define RHEL_MAJOR 7
-#define RHEL_MINOR 6
-#define RHEL_RELEASE_VERSION(a,b) (((a) << 8) + (b))
-#define RHEL_RELEASE_CODE 1798
-#define RHEL_RELEASE "957.21.3"
-#define KERNEL_HEADERS_CHECKSUM "7efa54f4b0a5b0f4548bea23ffbcf022c01e852d"
